bbc6fca2f46df34393356fcd02783e32676f473c
Stephen Banbury <sbanbury@gmail.com>
2019-11-12 01:10:10 +0000

    Merge two scenes

---
+++ conference-booth-games-1/Assets/EnterPrepRoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterPrepRoom : MonoBehaviour
+{
+    public Text DoorMessage;
+    private AudioSource audioSource1;
+    //private AudioSource audioSource2;
+
+    void Start()
+    {
+        var audioSources = GetComponents<AudioSource>();
+        audioSource1 = audioSources[0]; 
+        //audioSource2 = audioSources[1];
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            audioSource1.Play();
+            DoorMessage.text = "Welcome!";
+            GameManager.instance.LoadAppropriateScene("PreperationCentre");
+        } 
+    }
+}
---
+++ conference-booth-games-1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // static instance of the GM can be accessed from anywhere
+    public static GameManager instance;
+
+    void Awake()
+    {
+        print("Awake");
+        // Check that it exists
+        if (instance == null)
+        {
+            //assign it to the current object
+            instance = this;
+        }
+
+        // make sure that it is equal to the current object
+        else if (instance != this)
+        {
+            // Destroy current game object - we only need one and we already have it
+            Destroy(gameObject);
+        }
+
+        // don't destroy the object when changing scenes!
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("HomeTown");
+        SceneManager.LoadScene("HomeTown");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadAppropriateScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
