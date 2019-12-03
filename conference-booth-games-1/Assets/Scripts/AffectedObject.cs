fbbcdac9d2a5f37923830990e43266b72633ac98
Stephen Banbury <sbanbury@gmail.com>
2019-12-03 23:59:02 +0000

    Further disaster area refinement

--- conference-booth-games-1/Assets/Scripts/AffectedObject.cs
+++ conference-booth-games-1/Assets/Scripts/AffectedObject.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class AffectedObject : MonoBehaviour
     {
-        public float lifeTime = 10f;
+        //public Text debugText;
+        //public float lifeTime = 10f;
         public bool inWindZone = false;
         public GameObject windZone;
 
         private Rigidbody rb;
+        private Vector3 trans;
+        private GameObject go;
 
         private void Start()
         {
             windZone = GameObject.Find("Floor");
+            go = gameObject;
+            trans = gameObject.transform.position;
             rb = GetComponent<Rigidbody>();
-            print($"rb: {rb.name}");
+
+            //print($"rb: {rb.name}");
         }
 
         void Update()
@@ -31,15 +38,25 @@ namespace Assets.Scripts
             //    }
             //}
 
-            if (this.transform.position.y <= -20)
+            //debugText.text = $"x: {this.transform.position.x.ToString()}, y: {this.transform.position.y.ToString()}, z{this.transform.position.z.ToString()}";
+
+            if (this.transform.position.y <= -5)
             {
-                Destruction();
+                //Destruction();
+
+                //Instantiate(go, new Vector3(0.08f + randomPosition, 3.5f, 1.4f + randomPosition), Quaternion.identity);
+                //Instantiate(go, new Vector3(trans.x, trans.y, trans.z), Quaternion.identity);
+
+                this.transform.position = new Vector3(trans.x, trans.y, trans.z);
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.Sleep();
             }
         }
 
         void FixedUpdate()
         {
-            print($"inWindZone: {inWindZone.ToString()}");
+            //print($"inWindZone: {inWindZone.ToString()}");
 
             if (inWindZone)
             {
@@ -65,13 +82,13 @@ namespace Assets.Scripts
             }
         }
 
-        void OnCollisionEnter(Collision coll)
-        {
-            if (coll.gameObject.name == "destroyer")
-            {
-                Destruction();
-            }
-        }
+        //void OnCollisionEnter(Collision coll)
+        //{
+        //    if (coll.gameObject.name == "destroyer")
+        //    {
+        //        Destruction();
+        //    }
+        //}
 
         void Destruction()
         {
