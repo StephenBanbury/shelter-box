6eb6e5566e7b1a3a4e465d923ad8558635128c8e
Stephen Banbury <sbanbury@gmail.com>
2019-12-03 00:31:07 +0000

    First Experiment with rigidbody wind force

---
+++ conference-booth-games-1/Assets/Scripts/Ball.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Ball : MonoBehaviour
+    {
+
+        public float lifeTime = 10f;
+        public bool inWindZone = false;
+        public GameObject windZone;
+
+        private Rigidbody rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            if (lifeTime > 0)
+            {
+                lifeTime -= Time.deltaTime;
+                if (lifeTime <= 0)
+                {
+                    Destruction();
+                }
+            }
+
+            if (this.transform.position.y <= -20)
+            {
+                Destruction();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (inWindZone)
+            {
+                var windArea = windZone.GetComponent<WindArea>();
+                rb.AddForce(windArea.direction * windArea.strength);
+            }
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.gameObject.tag == "WindArea")
+            {
+                windZone = coll.gameObject;
+                inWindZone = true;
+            }
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.gameObject.tag == "WindArea")
+            {
+                inWindZone = false;
+            }
+        }
+
+        void OnCollisionEnter(Collision coll)
+        {
+            if (coll.gameObject.name == "destroyer")
+            {
+                Destruction();
+            }
+        }
+
+        void Destruction()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ conference-booth-games-1/Assets/Scripts/WindArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WindArea : MonoBehaviour
+    {
+        public float strength;
+        public Vector3 direction;
+    }
+}
