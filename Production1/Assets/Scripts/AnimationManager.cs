c97659803bedd3948286a505eb1f4b71a6409dca
Stephen Banbury <sbanbury@gmail.com>
2020-04-11 16:48:58 +0100

    AnimationManager

--- Production1/Assets/Scripts/AnimationManager.cs
+++ Production1/Assets/Scripts/AnimationManager.cs
@@ -66,16 +66,16 @@ namespace Com.MachineApps.PrepareAndDeploy
                         //monitorTextFade.SetBool("text2FadeIn", true);
                         break;
                     case "monitor3":
-                        monitor3.SetBool("closeMonitor", false);
+                       // monitor3.SetBool("closeMonitor", false);
                         monitor3.SetBool("openMonitor", true);
-                        monitorTextFade.SetBool("text3FadeOut", false);
-                        monitorTextFade.SetBool("text3FadeIn", true);
+                        //monitorTextFade.SetBool("text3FadeOut", false);
+                        //monitorTextFade.SetBool("text3FadeIn", true);
                         break;
                     case "monitor4":
-                        monitor4.SetBool("closeMonitor", false);
+                        //monitor4.SetBool("closeMonitor", false);
                         monitor4.SetBool("openMonitor", true);
-                        monitorTextFade.SetBool("text4FadeOut", false);
-                        monitorTextFade.SetBool("text4FadeIn", true);
+                        //monitorTextFade.SetBool("text4FadeOut", false);
+                        //monitorTextFade.SetBool("text4FadeIn", true);
                         break;
                 }
             }
@@ -99,15 +99,15 @@ namespace Com.MachineApps.PrepareAndDeploy
                         break;
                     case "monitor3":
                         monitor3.SetBool("openMonitor", false);
-                        monitor3.SetBool("closeMonitor", true);
-                        monitorTextFade.SetBool("text3FadeIn", false);
-                        monitorTextFade.SetBool("text3FadeOut", true);
+                        //monitor3.SetBool("closeMonitor", true);
+                        //monitorTextFade.SetBool("text3FadeIn", false);
+                        //monitorTextFade.SetBool("text3FadeOut", true);
                         break;
                     case "monitor4":
                         monitor4.SetBool("openMonitor", false);
-                        monitor4.SetBool("closeMonitor", true);
-                        monitorTextFade.SetBool("text4FadeIn", false);
-                        monitorTextFade.SetBool("text4FadeOut", true);
+                        //monitor4.SetBool("closeMonitor", true);
+                        //monitorTextFade.SetBool("text4FadeIn", false);
+                        //monitorTextFade.SetBool("text4FadeOut", true);
                         break;
                 }
             }
