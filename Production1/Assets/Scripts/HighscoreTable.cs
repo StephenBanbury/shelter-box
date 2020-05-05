50c53a112bafe41c3eb5ab4ee22f649967d1ffd5
Stephen Banbury <sbanbury@gmail.com>
2020-05-05 01:33:49 +0100

    Begin adding highscores table

---
+++ Production1/Assets/Scripts/HighscoreTable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Com.MachineApps.PrepareAndDeploy
+{
+    public class HighscoreTable : MonoBehaviour
+    {
+        private Transform entryContainer;
+        private Transform entryTemplate;
+        
+        private void Awake()
+        {
+            entryContainer = transform.Find("highscoreEntryContainer");
+            entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+            entryTemplate.gameObject.SetActive(false);
+        }
+    }
+}
